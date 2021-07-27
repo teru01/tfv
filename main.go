5cc85406b32afe4325ffb3cf3a47063524f0714e
ono-teruya1 <27873650+teru01@users.noreply.github.com>
2021-07-27 22:20:11 +0900

    not show already declared variables

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -17,7 +18,8 @@ var outputTemplate = `variable "%s" {
   description = ""
 }`
 
-var pattern = regexp.MustCompile(`var\.([^}")\[\],]*)`)
+var pattern = regexp.MustCompile(`var\.([^}")\[\],\s]*)`)
+var pattrnForExtractVariables = regexp.MustCompile(`variable\s*"(.*)"`)
 
 func main() {
 	app := &cli.App{
@@ -46,6 +48,7 @@ func printVariables(c *cli.Context) error {
 // カレントディレクトリのtfファイルからvar.fooを抽出し、変数名を返す
 func walkFiles() ([]string, error) {
 	vars := make(map[string]struct{})
+	varsDeclared := make([]string, 0)
 	entries, err := os.ReadDir("./")
 	if err != nil {
 		return nil, xerrors.Errorf("failed to readDir: %w", err)
@@ -58,22 +61,68 @@ func walkFiles() ([]string, error) {
 		if err != nil {
 			return nil, xerrors.Errorf("failed to open file: %w", err)
 		}
-		varsInFile, err := extractVars(file)
+		buf := &bytes.Buffer{}
+		tee := io.TeeReader(file, buf)
+
+		varsInFile, err := extractVars(tee)
 		if err != nil {
 			return nil, xerrors.Errorf("failed to extract vars: %w", err)
 		}
 		for _, v := range varsInFile {
 			vars[v] = struct{}{}
 		}
+		vd, err := extractAlreadyDeclared(buf)
+		if err != nil {
+			return nil, xerrors.Errorf("failed to extract already declared vars: %w", err)
+		}
+		varsDeclared = append(varsDeclared, vd...)
 		file.Close()
 	}
-	results := make([]string, len(vars))
+	results := make([]string, 0)
 	i := 0
 	for v := range vars {
-		results[i] = v
-		i++
+		var declared bool
+		for _, vd := range varsDeclared {
+			if v == vd {
+				declared = true
+				break
+			}
+		}
+		if !declared {
+			results = append(results, v)
+			i++
+		}
 	}
 	return results, nil
+}
+
+// extract foo from `variables "foo"` and return it
+func extractAlreadyDeclared(file io.Reader) ([]string, error) {
+	scanner := bufio.NewScanner(file)
+	var vars []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "variable") {
+			s := extractVariable(line)
+			if s != "" {
+				vars = append(vars, s)
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, xerrors.Errorf("failed to scan: %w", err)
+	}
+	return vars, nil
+}
+
+// impl extractVariable
+func extractVariable(line string) string {
+	matches := pattrnForExtractVariables.FindStringSubmatch(line)
+	if len(matches) == 0 {
+		return ""
+	}
+	return strings.TrimSpace(matches[1])
 }
 
 func extractVars(file io.Reader) ([]string, error) {
