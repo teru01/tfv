b20b13916fd98e6ad6670f4c4c9a9f7565981545
ono-teruya1 <27873650+teru01@users.noreply.github.com>
2023-02-04 16:37:55 +0900

    fix flag and options

--- cmd/tfv/main.go
+++ cmd/tfv/main.go
@@ -10,9 +10,29 @@ import (
 
 func main() {
 	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "dir",
+				Value: ".",
+				Usage: "tfv collects variables from `DIR`",
+			},
+			&cli.BoolFlag{
+				Name:  "sync",
+				Usage: "execute in sync mode (tfv generates variables without unused variables)",
+			},
+			&cli.StringFlag{
+				Name:  "tfvars-file",
+				Usage: "load tfvars from `FILE`",
+			},
+			&cli.StringFlag{
+				Name:  "suffix",
+				Value: ".generated",
+				Usage: "suffix of generated files",
+			},
+		},
 		Name:   "tfv",
-		Usage:  "generate Terraform variables declaration",
-		Action: core.PrintVariables,
+		Usage:  "Terraform variables generator",
+		Action: core.Execute,
 	}
 
 	err := app.Run(os.Args)
