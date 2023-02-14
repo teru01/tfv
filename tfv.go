308fa882c7c7d9eab31d4924a7a5d5f8b141c4db
ono-teruya1 <27873650+teru01@users.noreply.github.com>
2023-02-14 21:44:06 +0900

    refactor: delete unused func

--- tfv.go
+++ tfv.go
@@ -1,24 +1,13 @@
 package core
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
-	"sort"
 	"strings"
 
 	"github.com/urfave/cli/v2"
 )
-
-type tfVariable struct {
-	block string
-	used  bool
-	order int
-}
-
-type tfVariables map[string]tfVariable
 
 type tfvarBlock struct {
 	name  string
@@ -56,21 +45,6 @@ func GenerateVariables(ctx *cli.Context) (string, string, error) {
 	}
 
 	return variables, "", nil
-}
-
-func buildVariableString(vars tfVariables) string {
-	var ov []tfVariable
-	var ovs []string
-	for _, v := range vars {
-		ov = append(ov, v)
-	}
-	sort.Slice(ov, func(i, j int) bool {
-		return ov[i].order < ov[j].order
-	})
-	for _, v := range ov {
-		ovs = append(ovs, v.block)
-	}
-	return strings.Join(ovs, "\n\n")
 }
 
 func collectAllUsedVariables(dir string) (usedVariables, error) {
@@ -115,62 +89,4 @@ func rebuildVariableFile(usedVars usedVariables, path string, sync bool) (string
 	}
 
 	return declaredVars, unusedVariables, nil
-}
-
-func buildVariableBlocks(dir string) (tfVariables, error) {
-	variableBlocks := make(tfVariables)
-	usedVars := make(map[string]struct{})
-
-	entries, err := os.ReadDir(dir)
-	if err != nil {
-		return nil, fmt.Errorf("path %v: %w", dir, err)
-	}
-	for _, entry := range entries {
-		path := filepath.Join(dir, entry.Name())
-		if entry.IsDir() || !strings.HasSuffix(path, ".tf") {
-			continue
-		}
-		file, err := os.Open(path)
-		if err != nil {
-			return nil, fmt.Errorf("open %v: %w", path, err)
-		}
-		defer file.Close()
-
-		var copiedFile bytes.Buffer
-		teeFile := io.TeeReader(file, &copiedFile)
-
-		declaredVars, err := collectDeclaredVariables(teeFile)
-		if err != nil {
-			return nil, fmt.Errorf("collect declared variables: %w", err)
-		}
-		for k, v := range declaredVars {
-			variableBlocks[k] = v
-		}
-
-		vu, err := collectUsedVariables(&copiedFile)
-		if err != nil {
-			return nil, fmt.Errorf("collect used variables: %w", err)
-		}
-		for k := range vu {
-			usedVars[k] = struct{}{}
-		}
-	}
-
-	for used := range usedVars {
-		variable, ok := variableBlocks[used]
-		if ok {
-			variableBlocks[used] = tfVariable{
-				block: variable.block,
-				used:  true,
-				order: variable.order,
-			}
-		} else {
-			variableBlocks[used] = tfVariable{
-				block: fmt.Sprintf(outputTemplate, used),
-				used:  true,
-				order: variable.order,
-			}
-		}
-	}
-	return variableBlocks, nil
 }
